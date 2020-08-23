5a49fcaa49ae967db6679e5d588a62083066d37c
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 22:27:50 +0530

    Add files via upload

---
+++ DatatypesTutorial.java
@@ -0,0 +1,22 @@
+package dataTypes;
+
+public class DatatypesTutorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int age = 10;
+        
+        float rateOfInterest = 2.3f;
+        
+        double rate = 3.1428571;
+        
+        System.out.println(rate);
+        
+        boolean isThisSeriesCool = false;
+        
+        char myCharacter = '@';
+        System.out.println('@');
+        
+	}
+
+}
